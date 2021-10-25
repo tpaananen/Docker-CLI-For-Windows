1a31254c83312f380e2e7749de70df81f950f7ec
Tommi Paananen <tpaananen@users.noreply.github.com>
2021-10-25 14:07:55 +0300

    Update disable-wsl-network.cmd

--- disable-wsl-network.cmd
+++ disable-wsl-network.cmd
@@ -1,2 +1,5 @@
 :: run as administrator
 netsh interface set interface "vEthernet (WSL)" disable
+
+:: optionally uncheck the vpn item from the interface
+:: (powershell) Disable-NetAdapterBinding -Name "vEthernet (WSL)" -ComponentID sgra_vpn
