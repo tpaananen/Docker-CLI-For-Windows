3b62a305e113a83c0a9e7d95fd46a1bea270205e
Tommi Paananen <tpaananen@users.noreply.github.com>
2021-10-29 10:36:14 -0700

    Update docker-compose.cmd

--- docker-compose.cmd
+++ docker-compose.cmd
@@ -3,10 +3,7 @@
 set help=%1
 
 IF "%help%"=="--help" (
-  echo 1. Install docker-compose
-  echo 2. sudo apt-get update && sudo apt-get install docker-compose
-  echo 3. Here we assumed you have docker installed and running in WSL Debian based Linux
-  echo 4. Add this script or location of this script to PATH
+  echo Go to https://github.com/tpaananen/DockerCommandsForWindows
 ) ELSE (
   :: use -d distribution_name option for wsl.exe if not using the default distribution, e.g. wsl.exe -d Debian bash -i -c "docker-compose %*"
   wsl.exe bash -i -c "docker-compose %*"
